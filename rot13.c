700cb7d780bb21749bf6ee5a7f35b3b2622059e5
Dario <dario.castano@aim.com>
2019-07-29 17:10:28 +0000

    Add rot13.c to the repo

---
+++ rot13.c
@@ -0,0 +1,26 @@
+/**
+ * rot13 - traslate an array of chars to ROT13
+ * @arr: An array of chars
+ * Return: Translated array
+ */
+char *rot13(char *arr)
+{
+	int i = 0;
+	int j = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (arr[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (in[j] == arr[i])
+			{
+				arr[i] = out[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (arr);
+}
