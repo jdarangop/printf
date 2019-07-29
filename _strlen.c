9364f56b053a24a9860988019f384233cd700f54
jdarangop <juandiegoap33@gmail.com>
2019-07-29 17:12:18 +0000

    Add _strlen.c to the folder

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+/**
+  * _strlen - Return the len of a string.
+  * @s: The string.
+  * Return: Lenght of a string.
+  */
+
+int _strlen(char *s)
+{
+	int i;
+	
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
