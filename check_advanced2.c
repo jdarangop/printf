2f818a1822f7522b213a09472c80c307183062f9
jdarangop <juandiegoap33@gmail.com>
2019-07-30 21:47:26 +0000

    Fix bytes counter

--- check_advanced2.c
+++ check_advanced2.c
@@ -3,6 +3,7 @@
   * check_advanced2 - Function to excecute the switch.
   * @tmp: Character to compare.
   * @valist: List of arguments.
+  * @count: Counter.
   * Return: The i incremented.
   */
 
@@ -16,15 +17,15 @@ int check_advanced2(char tmp, va_list valist, int *count)
 		case 'X':
 			c = va_arg(valist, unsigned int);
 			_puts(convert_uint(c, 16, 1));
-			count += _strlen(convert_uint(c, 16, 1));
+			*count += _strlen(convert_uint(c, 16, 1));
 			return (1);
 		case 'p':
 			ul = va_arg(valist, unsigned long);
 			_putchar(48);
 			_putchar(120);
 			_puts(convert_ulong(ul, 16, 0));
-			count += _strlen(convert_ulong(ul, 16, 0));
-			count += 2;
+			*count += _strlen(convert_ulong(ul, 16, 0));
+			*count += 2;
 			return (1);
 		case '\0':
 			return (1);
